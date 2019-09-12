0f5e8c9c03f0d133544e3fcea8fc5c14ce5fb622
Fei <unshi@outlook.com>
2019-09-12 17:31:50 -0400

    updae

---
+++ 1Awork/OrionStar/OrionStar/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrionStar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ClickShow(object sender, RoutedEventArgs e) //make a event show informations when click
+        {
+            Label[] starts = { L1, L2, L3, L4, L5, L6, L7 }; //creat a group include all labels whcih named L1-L7
+            foreach (var item in starts) //use foreach make all items in labels visible
+            {
+                item.Visibility = Visibility.Visible;
+            }
+        }
+        private void ClickHide(object sender, RoutedEventArgs e) //make a event hide information when click
+        {
+            Label[] starts = { L1, L2, L3, L4, L5, L6, L7 }; //creat a group include all labels 
+            foreach (var item in starts) //use foreach make all items in labels hide
+            {
+                item.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ Week2/Week2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoIt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float s = float.Parse(FirstInput.Text);
+                float b = float.Parse(SecondInput.Text);
+                //string s = FirstInput.Text;
+                //string b = SecondInput.Text;
+                float m = s / b;
+                decimal money = (decimal)m;
+                OutputText.Content =money;
+            }
+            catch { MessageBox.Show("NO"); }
+        }
+    }
+}
