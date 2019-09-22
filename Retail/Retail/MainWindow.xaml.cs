524a18f4161166989055dc5541dd21215fd1a055
Fei <unshi@outlook.com>
2019-09-22 14:42:56 -0400

    up

---
+++ Retail/Retail/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Retail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static float Coupon(int num)
+        {
+            if (num < 10) { return 1.00f*num; }
+            else if (num >= 10 && num < 20) { return 0.80f*num; }
+            else if (num >= 20 && num < 50) { return 0.70f*num; }
+            else if (num >= 50 && num < 100) { return 0.60f*num; }
+            else { return 0.50f*num; }
+        }
+
+    private void Packs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int price = 99;
+           
+            try
+            {
+                int number;
+                int.TryParse(packs.Text, out number);
+                if (number>0)
+                {                  
+                    pricenow.Text = (Coupon(number) * price).ToString();
+                    pricewas.Text = (price * number).ToString();
+                    discount.Text = (100 * (number - Coupon(number)) / number).ToString() + "%";
+                }
+            }
+            catch { }
+                    
+        }
+    }
+}
